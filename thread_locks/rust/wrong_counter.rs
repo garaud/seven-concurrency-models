ca7a69ea6486404eadd6cbbbfb57861791f5c680
Damien Garaud <damien.garaud@gmail.com>
2015-02-17 23:38:21 +0100

    Keep trying Rust wrong counter: not work yet

--- thread_locks/rust/wrong_counter.rs
+++ thread_locks/rust/wrong_counter.rs
@@ -9,8 +9,16 @@ fn increment_loop(counter: &mut Counter) {
     }
 }
 
+fn intincr(value: &mut int) {
+    *value = *value + 1
+}
+
 fn main() {
-    let mut c : Counter = Counter { value: 0 };
-    increment_loop(&c);
-    println!("Counter value {}", c.value);
+    //let mut c : Counter = Counter { value: 0 };
+    //increment_loop(&c);
+    //println!("Counter value {}", c.value);
+
+    let mut x = 10i;
+    intincr(x);
+    println!("x value: {}", x);
 }
