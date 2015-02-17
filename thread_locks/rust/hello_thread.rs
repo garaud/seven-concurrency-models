8ca930b717661b3f90d5bf8eaa8bbc7df1cbf3cc
Damien Garaud <damien.garaud@gmail.com>
2015-02-17 23:39:47 +0100

    thread_locks: new Rust version based on the nighly 1.0.0 build

--- thread_locks/rust/hello_thread.rs
+++ thread_locks/rust/hello_thread.rs
@@ -1,8 +1,11 @@
+// Mainly inspired from official book at
+// http://doc.rust-lang.org/book/concurrency.html
 
+use std::thread::Thread;
 
 fn main() {
     println!("Hello from main");
-    spawn(proc() {
+    Thread::scoped(|| {
         println!("Hello from new thread")
     });
 }
