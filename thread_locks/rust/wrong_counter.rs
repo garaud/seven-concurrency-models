191a26aa67c3071302f24a3c51b1e39c041d3c41
Damien Garaud <damien.garaud@gmail.com>
2015-02-18 22:44:43 +0100

    [thread_locks] rust: fix errors and warns due to new API and deprecated stuff

--- thread_locks/rust/wrong_counter.rs
+++ thread_locks/rust/wrong_counter.rs
@@ -1,24 +1,33 @@
+// Example of a wrong threaded counter.
 
+// Reading this http://doc.rust-lang.org/book/concurrency.html, I think yon can
+// not share mutable state between threads (it does not compile).
+
+use std::thread::Thread;
+
+// Dumb counter
 struct Counter {
-    value : int
+    value : i32
 }
 
+// Function which increments the value by one.
 fn increment_loop(counter: &mut Counter) {
-    for n in range(0u, 5000u) {
+    for _ in 0u32..5000u32 {
         counter.value += 1
     }
 }
 
-fn intincr(value: &mut int) {
-    *value = *value + 1
-}
 
 fn main() {
-    //let mut c : Counter = Counter { value: 0 };
-    //increment_loop(&c);
-    //println!("Counter value {}", c.value);
+    let mut counter : Counter = Counter { value: 0 };
+    // First thread.
+    Thread::spawn(move || {
+        increment_loop(&mut counter);
+    });
+    // increment_loop(&mut counter);
+    println!("Counter value {}", counter.value);
 
-    let mut x = 10i;
-    intincr(x);
-    println!("x value: {}", x);
+    // let mut x = 10;
+    // x = incr(x);
+    // println!("x value: {}", x);
 }
