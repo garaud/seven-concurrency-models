8aeb6f63ee8d4657fd7d8a88962649ed84b28f2c
Damien Garaud <damien.garaud@gmail.com>
2015-02-19 16:28:35 +0100

    [thread_locks] add 'hello' thread example in Python

---
+++ thread_locks/python/hello_thread.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+"""Simple 'hello' thread example.
+"""
+
+from __future__ import print_function
+import threading
+
+
+def hello():
+    print("Hello from new thread")
+
+
+def main():
+    print("Hello from main")
+    threading.Thread(target=hello).start()
+
+if __name__ == '__main__':
+    main()
